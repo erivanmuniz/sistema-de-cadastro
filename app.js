4c1bc8f4337ff22414225a26d0882a276251d0d2
Erivan <erivanmuniz@outlook.com>
2023-08-09 08:26:48 -0300

    Voltando para o estado das duas alterações feitas pelo Erivan

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação está se conectando ao banco de dados")
 //mais um console log de testes:
 console.log("O Erivan fez esse comit sem você ver na aula!")
-console.log("Enviando para o Github")
